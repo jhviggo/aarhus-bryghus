7c54791faa7c4ad55749836225e88721763a245f
Niklas <niklasmknudsen@hotmail.com>
2019-10-17 11:53:11 +0200

    PriceList now contains methods to get and setPrice

--- src/model/PriceList.java
+++ src/model/PriceList.java
@@ -1,5 +1,30 @@
 package model;
 
+import java.util.HashMap;
+
 public class PriceList {
 
+	/**
+	 * Attributes
+	 */
+	private String type;
+	private double prices;
+	HashMap<Product, Double> productsInPriceList;
+	/**
+	 * Method to set price on product.
+	 * @param product
+	 */
+	public void setPrice(Product product, double price) {
+		productsInPriceList.put(product, price);
+	}
+	
+	/**
+	 * Get method.
+	 * Method to get product price.
+	 * @param product
+	 * @return product price.
+	 */
+	public double getPrice(Product product) {
+	    return productsInPriceList.get(product);
+	}
 }
