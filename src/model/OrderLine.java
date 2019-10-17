a9b5d410626b309bbc31ceb9ad85e0872f92305a
Hans Christian Tankred <teri@akroma.localdomain>
2019-10-17 11:48:57 +0200

    Implemented OrderLine class

--- src/model/OrderLine.java
+++ src/model/OrderLine.java
@@ -7,21 +7,46 @@ public class OrderLine {
 	 */
 	private PriceList priceList;
 	private Product product;
+	private int amount;
 	
 	/**
 	 * Constructor
 	 * @param product
 	 */
-	public OrderLine(Product product) {
+	public OrderLine(Product product, PriceList priceList, int amount) {
 		this.product = product;
+		this.priceList = priceList;
+		this.amount = amount;
 	}
+
+
+	/**
+	 * Gets amount of product
+	 * @return amount
+	 */
+	public int getAmount() {
+		return amount;
+	}
+
+
+	/**
+	 * Sets amount of product
+	 * @param amount
+	 */
+	public void setAmount(int amount) {
+		this.amount = amount;
+	}
+
 	/**
 	 * Get method.
 	 * Get total prices from priceList.
 	 * @return total price.
 	 */
 	public double getPrice() {
-		return 0.0;
+		double sum = 0.0;
+		for (int i = 0; i < amount; i++) {
+			sum += priceList.getPrice(product);
+		}
 	}
 	
 }
