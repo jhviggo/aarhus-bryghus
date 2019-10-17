84a1ba91504ba58e9761da07d0280bf92c0de484
Viggo Petersen <viggo.petersen@sallinggroup.com>
2019-10-17 11:54:45 +0200

    Add ProductGroup
    solves #10

--- src/model/Product.java
+++ src/model/Product.java
@@ -3,9 +3,11 @@ package model;
 public class Product {
 
 	private String productName;
+	private ProductGroup productGroup;
 
-	public Product(String productName) {
+	public Product(String productName, ProductGroup productGroup) {
 		this.productName = productName;
+		this.productGroup = productGroup;
 	}
 
 	public String getProductName() {
