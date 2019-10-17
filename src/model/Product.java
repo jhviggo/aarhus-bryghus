9696e888ced8546d8b36c863b5ce8caacb46d09a
Niklas <niklasmknudsen@hotmail.com>
2019-10-17 09:37:25 +0200

    added private string productname

--- src/model/Product.java
+++ src/model/Product.java
@@ -2,6 +2,8 @@ package model;
 
 public abstract class Product {
 
+	private String productName;
+	
 	public abstract String getProductName();
 	
 	public abstract String setProductName(String name);
