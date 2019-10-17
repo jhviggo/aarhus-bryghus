7343dd5e9f0da6f1c86dbe8390c6331929e8d4ab
Niklas <niklasmknudsen@hotmail.com>
2019-10-17 10:54:08 +0200

    Order & Orderline Attributes and methods has been added with comments

--- src/model/Order.java
+++ src/model/Order.java
@@ -1,5 +1,106 @@
 package model;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+
+import model.OrderLine;
+
 public class Order {
 
+	/**
+	 * Attributes.
+	 */
+	private int ID;
+	private LocalDateTime startTimestamp;
+	private LocalDateTime endTimestamp;
+	private OrderStatusType status;
+	private ArrayList<OrderLine> orderline;
+	
+	/**
+	 * Constructor
+	 * @param ID, startTimestamp, endTimestamp, status
+	 */
+	public Order(int id, LocalDateTime startTimestamp, LocalDateTime endTimestamp, OrderStatusType status) {
+		this.ID = id;
+		this.startTimestamp = startTimestamp;
+		this.endTimestamp = endTimestamp;
+		this.status = status;
+		this.orderline = new ArrayList<>();
+	}
+
+	/**
+	 * Get method.
+	 * Method to get endTimestamp value.
+	 * @return endTimestamp
+	 */
+	public LocalDateTime getEndTimestamp() {
+		return endTimestamp;
+	}
+
+	/**
+	 * Set method.
+	 * set endTimestamp attribute value.
+	 * @param endTimestamp
+	 */
+	public void setEndTimestamp(LocalDateTime endTimestamp) {
+		this.endTimestamp = endTimestamp;
+	}
+
+	/**
+	 * Get method
+	 * Method to get order status
+	 * @return status
+	 */
+	public OrderStatusType getStatus() {
+		return status;
+	}
+
+	/**
+	 * Set method.
+	 * set status attribute value.
+	 * @param status
+	 */
+	public void setStatus(OrderStatusType status) {
+		this.status = status;
+	}
+
+	/**
+	 * Get method.
+	 * Method to get order ID.
+	 * @return ID
+	 */
+	public int getID() {
+		return ID;
+	}
+	/**
+	 * Get method.
+	 * Method to get start time stamp. 
+	 * @return startTimestamp
+	 */
+	public LocalDateTime getStartTimestamp() {
+		return startTimestamp;
+	}
+	
+	/**
+	 * Method to create a new OrderLine.
+	 * @return a new instance of OrderLine.
+	 */
+	public OrderLine createOrderLine(this, Product product) {
+		OrderLine orderLine = new OrderLine(this, product);
+		orderline.add(orderLine);
+		return orderLine;
+	}
+	/**
+	 * Method to delete a OrderLine.
+	 * @param order
+	 */
+	public void deleteOrderline(OrderLine order) {
+		if (orderline.contains(order)) {
+			orderline.remove(order);
+			order.removeOrder();
+		}
+	}
+	
+	
+	
 }
--- src/model/OrderLine.java
+++ src/model/OrderLine.java
@@ -2,4 +2,33 @@ package model;
 
 public class OrderLine {
 
+	/**
+	 * Attributes
+	 */
+	private Order order;
+	private PriceList priceList;
+	private Product product;
+	
+	/**
+	 * Constructor
+	 * @param product
+	 */
+	public OrderLine(Product product) {
+		this.product = product;
+	}
+	/**
+	 * Get method.
+	 * Get total prices from priceList.
+	 * @return total price.
+	 */
+	public double getPrice() {
+		return 0.0;
+	}
+	
+	/**
+	 * Method to set order to null value.
+	 */
+	public void removeOrder() {
+		this.order = null;
+	}
 }
