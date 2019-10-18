c79f327b37f24d6994e906d7222a8788ac82eeb1
Viggo Petersen <viggo.petersen@sallinggroup.com>
2019-10-18 08:46:57 +0200

    Implement DraughtBeerSystem
    solves #3

--- src/model/DraughtBeerSystem.java
+++ src/model/DraughtBeerSystem.java
@@ -1,5 +1,14 @@
 package model;
 
-public class DraughtBeerSystem {
+public class DraughtBeerSystem extends Product {
+    private int numberOfTaps;
 
+    public DraughtBeerSystem(String productName, ProductGroup productGroup, int numberOfTaps) {
+        super(productName, productGroup);
+        this.numberOfTaps = numberOfTaps;
+    }
+
+    public int getNumberOfTaps() {
+        return numberOfTaps;
+    }
 }
