7159c15576fba77f481badf5b1255553c84a9624
Hans Christian Tankred <teri@akroma.localdomain>
2019-10-21 09:06:45 +0200

    Initial GUI setup

---
+++ src/gui/Main.java
@@ -0,0 +1,55 @@
+package gui;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TabPane;
+import javafx.scene.control.TabPane.TabClosingPolicy;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    public static void main(String[] args) {
+        Application.launch(args);
+    }
+
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public void start(Stage stage) {
+        stage.setTitle("Aarhus Bryghus - POS System");
+        BorderPane pane = new BorderPane();
+        this.initContent(pane);
+
+        Scene scene = new Scene(pane);
+        stage.setScene(scene);
+        stage.setHeight(800);
+        stage.setWidth(800);
+        stage.show();
+    }
+
+    private void initContent(BorderPane pane) {
+        TabPane tabPane = new TabPane();
+        this.initTabPane(tabPane);
+        pane.setCenter(tabPane);
+    }
+
+    private void initTabPane(TabPane tabPane) {
+        tabPane.setTabClosingPolicy(TabClosingPolicy.UNAVAILABLE);
+
+        Tab tabOrder = new Tab("Register sale");
+        Tab tabRegister = new Tab("Register new product");
+        Tab tabPriceList = new Tab("Manage pricelists");
+        //Tab udtræk
+        //Tab ikke aflevere produkter
+        //Tab rundvisning
+
+        tabPane.getTabs().add(tabOrder);
+        tabPane.getTabs().add(tabRegister);
+        tabPane.getTabs().add(tabPriceList);
+    }
+}
