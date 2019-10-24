8983a25cace9008df471c79459539f5eb35f85cf
Niklas <niklasmknudsen@hotmail.com>
2019-10-24 23:11:58 +0200

    Product Tour now has been created with components

--- src/gui/ProductTour.java
+++ src/gui/ProductTour.java
@@ -2,10 +2,13 @@ package gui;
 
 import javafx.geometry.Insets;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import model.PriceList;
 
 public class ProductTour {
 	
@@ -17,8 +20,8 @@ public class ProductTour {
 	private Label lblMinut;
 	private Button btnCreate;
 	private DatePicker dpDato;
-	private TextField txtHour;
-	private TextField txtMin;
+	private ComboBox<String> cmbHour;
+	private ComboBox<String> cmbMin;
 	
 	/**
 	 * Product Tour Constructor
@@ -27,6 +30,10 @@ public class ProductTour {
 	 */
 	public ProductTour(GridPane grid) {
         grid.setPadding(new Insets(20));
+        String[] hours = {"00", "01", "02","03","04","05","06","07","08","09","10","11",
+                          "12","13","14","15","16","17","18","19","20","21","22","23"};
+        String[] minuts = {"00","15","30","45"};
+        
         
         // Label for DatePicker.
         lblDato = new Label("Choose date:");
@@ -36,6 +43,28 @@ public class ProductTour {
         dpDato = new DatePicker();
         grid.add(dpDato, 0, 1);
         
+        // creates a horizontal box used as container,
+        // for time selection.
+        HBox container = new HBox();
+        grid.add(container, 0, 2, 3, 4);
+        
+        // Label for timestamp
+        lblHour = new Label("Choose start hour:");
+        
+        // combo box hour
+        cmbHour = new ComboBox<>();
+        cmbHour.setPrefWidth(175);
+        cmbHour.getItems().addAll(hours);
+        
+        // combo box min
+        cmbMin = new ComboBox<>();
+        cmbMin.setPrefWidth(175);
+        cmbMin.getItems().addAll(minuts);
+        
+        // appends each component to container. 
+        container.getChildren().add(lblHour);
+        container.getChildren().add(cmbHour);
+        container.getChildren().add(cmbMin);
         
         
         btnCreate = new Button("Opret");
