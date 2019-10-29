cb0343aa9bb48a12233fa7af8acba415dcf4a3a1
Niklas <niklasmknudsen@hotmail.com>
2019-10-29 10:03:40 +0100

    changed constructor removed 2 paramters

--- src/model/ClipCard.java
+++ src/model/ClipCard.java
@@ -2,39 +2,15 @@ package model;
 
 import java.time.LocalDateTime;
 
-public class ClipCard {
-
-	/**
-	 * Attributes
-	 */
-	private int amountOfClips;
-	private LocalDateTime boughtOnDate;
+public class ClipCard extends Product {
 
 	/**
 	 * Contstructor
-	 * @param amountOfClips
-	 * @param boughtOnDate
 	 */
-	public ClipCard(int amountOfClips, LocalDateTime boughtOnDate) {
-		this.amountOfClips = amountOfClips;
-		this.boughtOnDate = boughtOnDate;
+	public ClipCard(String productName, ProductGroup productGroup) {
+		super(productName, productGroup);
 	}
-	
-	/**
-	 * Method to get amount of clips left on card.
-	 * @return amountOfClips
-	 */
-	public int getAmountOfClipsLeft() {
-		return this.amountOfClips;
-	}
-	
-	/**
-	 * Method to get the purchase date
-	 * @return boughtOnDate
-	 */
-	public LocalDateTime getDateofPurchase() {
-		return this.boughtOnDate;
-	}
+
 	
 	
 }
