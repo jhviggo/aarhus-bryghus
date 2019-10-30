2fd82bd1fd95d8cd2d3d4592107daa542e183d38
Niklas <niklasmknudsen@hotmail.com>
2019-10-30 10:30:34 +0100

    Changes in draughtbeersystem class new paramters startDate - endDate

--- src/model/DraughtBeerSystem.java
+++ src/model/DraughtBeerSystem.java
@@ -1,15 +1,67 @@
 package model;
 
-public class DraughtBeerSystem extends Product {
-    private int numberOfTaps;
+import java.time.LocalDateTime;
 
+public class DraughtBeerSystem extends Product {
+    
+	/**
+	 * Attributes
+	 */
+	private int numberOfTaps;
+    private LocalDateTime startDate;
+    private LocalDateTime endDate;
+    
+    /**
+     * Constructor
+     * @param productName
+     * @param productGroup
+     * @param numberOfTaps
+     * @param startDate
+     * @param endDate
+     */
     public DraughtBeerSystem(String productName, ProductGroup productGroup, int numberOfTaps) {
         super(productName, productGroup);
         this.numberOfTaps = numberOfTaps;
     }
 
+    /**
+     * Method to get number of taps
+     * @return numberOfTaps
+     */
     public int getNumberOfTaps() {
         return numberOfTaps;
+    }
+    
+    /**
+     * Method to set value of start date for rent
+     * @param startDate
+     */
+    public void setStartDate(LocalDateTime startDate) {
+    	this.startDate = startDate;
+    }
+    
+    /**
+     * Method to get start date of rent
+     * @return startDate
+     */
+    public LocalDateTime getStartDate() {
+    	return this.startDate;
+    }
+    
+    /**
+     * Method to set end date of rent
+     * @param endDate
+     */
+    public void setEndDate(LocalDateTime endDate) {
+    	this.endDate = endDate;
+    }
+    
+    /**
+     * Method to get end date of rent
+     * @return
+     */
+    public LocalDateTime getEndDate() {
+    	return this.endDate;
     }
 
     @Override
