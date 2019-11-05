dd4556026ba832deb6f9ec98a0091f17f70ee565
Niklas <niklasmknudsen@hotmail.com>
2019-11-05 09:43:23 +0100

    Testcase setup for 2 - 3

--- src/test/TestOrder.java
+++ src/test/TestOrder.java
@@ -12,7 +12,9 @@ import model.OrderStatusType;
 
 public class TestOrder {
 
-	private Order newOrder;
+	private Order testOrder1;
+	private Order testOrder2;
+	private Order testOrder3;
 
 	@Before
 	public void setup() {
@@ -22,7 +24,12 @@ public class TestOrder {
 	public TestOrder() {
 		int id = 1;
 		LocalDateTime startTimeStamp = LocalDateTime.of(2019, Month.JANUARY, 1, 12, 00);
-		newOrder = new Order(id, startTimeStamp, OrderStatusType.DONE);
+		testOrder1 = new Order(id, startTimeStamp, OrderStatusType.CREATED);
+		
+		testOrder2 = new Order(id, startTimeStamp, OrderStatusType.PROGRESS);
+		
+		testOrder3 = new Order(id, startTimeStamp, OrderStatusType.DONE);
+		
 	}
 
 	/**
@@ -33,6 +40,26 @@ public class TestOrder {
 	@Test
 	public void testCase1() {
 		assertEquals(1.0 , newOrder.getID(), 0.001);
+	}
+	
+	/**
+	 * TestCase 2: Basic Data
+	 * id = 1
+	 * startTimeStamp = ”2019-01-01”
+	 */
+	@Test
+	public void testCase2() {
+		
+	}
+	
+	/**
+	 * TestCase 3: Basic Data
+	 * id = 1
+	 * startTimeStamp = ”2019-01-01”
+	 */
+	@Test
+	public void testCase3() {
+		
 	}
 
 }
