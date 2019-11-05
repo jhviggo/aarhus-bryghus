17a903b3fb588f414bc9373eeaa74d5bf897d905
Niklas <niklasmknudsen@hotmail.com>
2019-11-05 09:49:51 +0100

    TestCases for Order has been completed, all passes tests STATE OK

--- src/test/TestOrder.java
+++ src/test/TestOrder.java
@@ -15,21 +15,17 @@ public class TestOrder {
 	private Order testOrder1;
 	private Order testOrder2;
 	private Order testOrder3;
-
-	@Before
-	public void setup() {
-		
-	}
 	
 	public TestOrder() {
+		// Basic Data
 		int id = 1;
 		LocalDateTime startTimeStamp = LocalDateTime.of(2019, Month.JANUARY, 1, 12, 00);
+		// TestCase 1:
 		testOrder1 = new Order(id, startTimeStamp, OrderStatusType.CREATED);
-		
+		// TestCase 2:
 		testOrder2 = new Order(id, startTimeStamp, OrderStatusType.PROGRESS);
-		
+		// TestCase 3:
 		testOrder3 = new Order(id, startTimeStamp, OrderStatusType.DONE);
-		
 	}
 
 	/**
@@ -39,7 +35,8 @@ public class TestOrder {
 	 */
 	@Test
 	public void testCase1() {
-		assertEquals(1.0 , newOrder.getID(), 0.001);
+		assertEquals(1.0 , testOrder1.getID(), 0.001);
+		// State : OK
 	}
 	
 	/**
@@ -49,17 +46,20 @@ public class TestOrder {
 	 */
 	@Test
 	public void testCase2() {
-		
+		assertEquals(1.0, testOrder2.getID(), 0.001);
+		// State : OK
 	}
 	
 	/**
 	 * TestCase 3: Basic Data
 	 * id = 1
 	 * startTimeStamp = ”2019-01-01”
+	 * endTimeStamp = "2019-01-01"
 	 */
 	@Test
 	public void testCase3() {
-		
+		assertEquals(1.0, testOrder3.getID(), 0.001);
+		// State : OK
 	}
 
 }
