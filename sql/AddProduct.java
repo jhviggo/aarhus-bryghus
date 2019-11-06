777958f3ae07056d69e7f4b9029b352dc01e2078
Viggo Petersen <viggo.petersen@sallinggroup.com>
2019-11-06 21:15:39 +0100

    Add assignment 6b
    - add query in sql
    - add java code

--- sql/AddProduct.java
+++ sql/AddProduct.java
@@ -14,49 +14,83 @@ public class AddProduct {
         try {
             myConnection = DriverManager.getConnection("jdbc:sqlserver://localhost:1433;databaseName=bryghus;user=sa;password=Viggo-12345;");
 
+            Statement stmt = myConnection.createStatement();
             BufferedReader inLine = new BufferedReader(new InputStreamReader(System.in));
 
-            System.out.println("Welcome to the interface for adding new products.");
-            System.out.print("Product name: ");
-            String productName = inLine.readLine();
-            System.out.println("Write ? to get product groups");
-            System.out.print("Product group: ");
-            String productGroup = inLine.readLine();
+            System.out.println("Would you like to add a product (add)");
+            System.out.println("or get total sales on a date(date)");
 
-            if (productGroup.equals("?")) {
-                Statement stmt = myConnection.createStatement();
-                ResultSet res = stmt.executeQuery("select productType from ProductGroup");
+            String choice = inLine.readLine();
 
-                String productGroupsPrint = "";
+            if (choice.trim().equalsIgnoreCase("add")) {
+                /**
+                 * Opgave 6a
+                 */
+                System.out.println("Welcome to the interface for adding new products.");
+                System.out.print("Product name: ");
+                String productName = inLine.readLine();
+                System.out.println("Write ? to get product groups");
+                System.out.print("Product group: ");
+                String productGroup = inLine.readLine();
 
-                while (res.next()) {
-                    productGroupsPrint += res.getString(1) + ", ";
+                if (productGroup.equals("?")) {
+                    ResultSet res = stmt.executeQuery("select productType from ProductGroup");
+
+                    String productGroupsPrint = "";
+
+                    while (res.next()) {
+                        productGroupsPrint += res.getString(1) + ", ";
+                    }
+                    System.out.println(productGroupsPrint.substring(0, productGroupsPrint.length()-2));
+
+                    productGroup = inLine.readLine();
                 }
-                System.out.println(productGroupsPrint.substring(0, productGroupsPrint.length()-2));
 
-                productGroup = inLine.readLine();
-            }
+                if (productGroup.trim().equals("")) {
+                    throw new RuntimeException("Product group cannot be an empty string");
+                }
+                else if (productName.trim().equals("")) {
+                    throw new RuntimeException("Product name cannot be an empty string");
+                }
 
-            if (productGroup.trim().equals("")) {
-                throw new RuntimeException("Product group cannot be an empty string");
-            }
-            else if (productName.trim().equals("")) {
-                throw new RuntimeException("Product name cannot be an empty string");
-            }
+                String sql = "INSERT INTO Product VALUES (?,?)";
 
-            String sql = "INSERT INTO Product VALUES (?,?)";// preparedStatement
+                PreparedStatement prestmt = myConnection.prepareStatement(sql);
+                prestmt.clearParameters();
 
-            PreparedStatement prestmt = myConnection.prepareStatement(sql);
-            prestmt.clearParameters();
+                prestmt.setString(1, productName.trim());
+                prestmt.setString(2, productGroup.trim());
 
-            prestmt.setString(1, productName.trim());
-            prestmt.setString(2, productGroup.trim());
+                prestmt.executeUpdate();
+                System.out.println("Done!");
 
-            prestmt.executeUpdate();
-            System.out.println("Done!");
-
-            if (prestmt != null)
                 prestmt.close();
+            }
+            else if (choice.trim().equalsIgnoreCase("date")) {
+                System.out.println("(date 2019-11-4 and product 1 works as an example)");
+                System.out.println("Date (yyyy-mm-dd): ");
+                String date = inLine.readLine();
+
+                if (date.trim().equals("")) {
+                    throw new RuntimeException("Date cannot be an empty string");
+                }
+
+                System.out.println("Product id: ");
+                String productId = inLine.readLine();
+
+                if (productId.trim().equals("")) {
+                    throw new RuntimeException("Product id cannot be an empty string");
+                }
+
+                String query = "SELECT SUM(pip.price * ol.amount) FROM ProductInPriceList pip, OrderLine ol, Orders o WHERE ol.orderId = o.id AND ol.product = pip.product AND pip.priceList = ol.priceList AND DATEDIFF(dd, o.startTimeStamp,'" + date + "') = 0 AND pip.product = " + productId;
+                ResultSet res = stmt.executeQuery(query);
+                System.out.print("Total income for this product on " + date + ": ");
+                while (res.next()) {
+                    System.out.println(res.getString(1) + "kr");
+                }
+
+            }
+
             if (myConnection != null)
                 myConnection.close();
         } catch (SQLException e) {
@@ -69,8 +103,12 @@ public class AddProduct {
             else if (e.getErrorCode() == 8152) {
                 System.out.println("Text is too long");
             }
+            else if (e.getErrorCode() == 102) {
+                System.out.println("Syntax error! Stop touching our pretty code :(");
+            }
             else {
                 System.out.println("Code: " + e.getErrorCode());
+                System.out.println(e.getMessage());
             }
         } catch (RuntimeException e) {
             System.out.println(e);
