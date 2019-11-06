f2f19276d1e6a0ea3c02338b708c56c034fb02ad
Niklas <niklasmknudsen@hotmail.com>
2019-11-06 09:17:00 +0100

    created file testProduct to test product model

---
+++ src/test/TestProduct.java
@@ -0,0 +1,41 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import model.Product;
+import model.ProductGroup;
+
+public class TestProduct {
+
+	private ProductGroup productgroup1;
+	private ProductGroup productgroup2;
+	private Product product1;
+	private Product product2;
+	
+	
+	public TestProduct() {
+		this.productgroup1 = new ProductGroup("flaske øl", 0);
+		this.productgroup2 = new ProductGroup("fustage", 0);
+		this.product1 = new Product("flaske øl", productgroup1);
+		this.product2 = new Product("fustage", productgroup2);
+	}
+	
+	@Test
+	public void testSetProductGroupTC1() {
+		
+	}
+
+	@Test
+	public void testSetProductGroupTC2() {
+		
+	}
+	
+	@Test
+	public void testSetProductGroupTC3() {
+		
+	}
+}
